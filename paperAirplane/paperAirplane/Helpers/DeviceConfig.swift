9bbd52b511c292761c2f24c207a607da6427b61a
2dubu <dlrjsdn5012@naver.com>
2022-06-20 00:03:30 +0900

    노치 여부 구별 오류 수정

--- paperAirplane/paperAirplane/Helpers/DeviceConfig.swift
+++ paperAirplane/paperAirplane/Helpers/DeviceConfig.swift
@@ -30,10 +30,8 @@ class DeviceManager {
 }
 
 var hasNotch: Bool {
-    if #available(iOS 11.0, tvOS 11.0, *) {
-        return UIApplication.shared.delegate?.window??.safeAreaInsets.top ?? 0 > 20
-    }
-    return false
+    let bottom = UIApplication.shared.keyWindow?.safeAreaInsets.bottom ?? 0
+    return bottom > 0
 }
 
 func checkDeviceNetworkStatus() -> Bool {
